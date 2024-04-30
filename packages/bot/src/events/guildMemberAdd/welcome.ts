e6668eb9f748760b62ef787668d175ad953e1921
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-04-30 17:53:45 -0300

    fix error valid type

--- packages/bot/src/events/guildMemberAdd/welcome.ts
+++ packages/bot/src/events/guildMemberAdd/welcome.ts
@@ -1,10 +1,10 @@
 import { type GuildMember } from 'discord.js'
-import { type Canvas } from '@/types/Canvas.types'
-import { readFileSync } from 'node:fs'
-import path from 'node:path'
 import db from '../../db'
 import SendWelcomeWith from '../../shared/sendWelcomeWith'
 import { userSecuencies } from '../../shared/messageFormatting'
+import WELCOME from '../../const/welcome'
+import { validateCanvas } from '../../commands/setWelcome/validate'
+import { type Canvas } from '@/types/Canvas.types'
 
 export default async function welcome(member: GuildMember): Promise<void> {
   const welcomeDb = await db.welcomeCommand.findUnique({
@@ -12,9 +12,10 @@ export default async function welcome(member: GuildMember): Promise<void> {
   })
   if (!welcomeDb) return
   const { channelId, image, message, send } = welcomeDb
-  const imageMock: Canvas = JSON.parse(readFileSync(path.join(__dirname, '../../../mocks/welcome.json'), 'utf-8'))
+  const isValidCanvas = validateCanvas(image)
+  const canvas = isValidCanvas ? (image as unknown as Canvas) : null
   const webhook = member.guild.channels.cache.get(channelId) as any
   await webhook?.send(
-    await SendWelcomeWith({ send, image: image ?? imageMock, message: userSecuencies(message, member), member })
+    await SendWelcomeWith({ send, image: canvas ?? WELCOME, message: userSecuencies(message, member), member })
   )
 }
