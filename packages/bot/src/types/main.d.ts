3ed606657792a78ee3d3216021194742f5d3bfed
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-01 16:25:34 -0300

    delete code dead

--- packages/bot/src/types/main.d.ts
+++ packages/bot/src/types/main.d.ts
@@ -1,19 +1,1 @@
-import type { Client, Collection, PermissionResolvable } from 'discord.js'
-import { type CommandNames } from '../const/CommandNames'
-import type { BaseFileButton, BaseFileCommand } from './BaseFiles'
-import type { ButtonNames } from '../const/ButtonNames'
-
-export interface ClientCustom extends Client {
-  commands: Collection<CommandNames | string, BaseFileCommand>
-  cooldowns: Collection<string, Collection<string, number>>
-  buttons: Collection<keyof typeof ButtonsNames | string, BaseFileButton>
-}
 export type Scope = 'public' | 'private' | 'owner'
-export interface BaseEventInteractionCreate {
-  name: CommandNames | ButtonNames
-  type: 'command' | 'button'
-  scope: 'public' | 'private' | 'owner'
-  cooldown?: number
-  ephemeral: boolean
-  permissions: PermissionResolvable[]
-}
