0b6dea531b0658c070bf7ec40295b3257820f004
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-02 17:58:52 -0300

    refactoring load of events

--- packages/bot/src/client.ts
+++ packages/bot/src/client.ts
@@ -1,11 +1,10 @@
-import fs from 'node:fs'
-import path from 'node:path'
 import { Client, Collection, GatewayIntentBits } from 'discord.js'
 import config from './config'
 import BuildCollection from './buildCollection'
 import deployCommand from './deployCommands'
 import BuildCommand from './shared/BuildCommand'
 import BuildButton from './shared/BuildButtons'
+import BuildEvent, { type Event } from './shared/BuildEvent'
 
 export default async function startClient(): Promise<void> {
   const client = new Client({
@@ -17,18 +16,14 @@ export default async function startClient(): Promise<void> {
   globalThis.cooldowns = new Collection()
   await deployCommand(globalThis.commands)
 
-  const eventsPath = path.join(__dirname, 'events')
-  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js') || file.endsWith('.ts'))
+  const events = await BuildCollection<string, Event>('events', BuildEvent)
 
-  for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file)
-    const event = require(filePath).default
+  events.forEach(event => {
     if (event.once) {
       client.once(event.name, (...args) => event.execute(...args))
     } else {
       client.on(event.name, (...args) => event.execute(...args))
     }
-  }
-
+  })
   client.login(config.discordToken)
 }
