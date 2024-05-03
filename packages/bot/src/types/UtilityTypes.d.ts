9262a693284987ad905f5489766540516875e43e
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-03 18:02:16 -0300

    delete types deprecated

--- packages/bot/src/types/UtilityTypes.d.ts
+++
@@ -1,8 +0,0 @@
-import type { InteractionEditReplyOptions, MessagePayload } from 'discord.js'
-import type { CustomButtonInteraction, CustomCommandInteraction } from './InteractionsCreate'
-export type EditReplyConstructor = InteractionEditReplyOptions | MessagePayload
-interface InteractionProperty {
-  interaction: CustomCommandInteraction | CustomButtonInteraction
-}
-
-export type EditReplyConstructorFn<T = object> = (key: T & InteractionProperty) => EditReplyConstructor | object
