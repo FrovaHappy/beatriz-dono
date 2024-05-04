e6e839ff8a9cdac64a49e678e1c10cc4455c7257
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-04 16:05:39 -0300

    implement ecxecuteMenu

--- packages/bot/src/events/interactionsCreate/index.ts
+++ packages/bot/src/events/interactionsCreate/index.ts
@@ -2,12 +2,17 @@ import { Events } from 'discord.js'
 import executeCommand from './executeCommand'
 import executeButton from './executeButton'
 import BuildEvent from '@core/build/BuildEvent'
+import executeMenu from './executeMenu'
 export default new BuildEvent({
   name: Events.InteractionCreate,
   once: false,
   async execute(interaction) {
     if (interaction.isChatInputCommand()) {
       await executeCommand(interaction)
+      return
+    }
+    if (interaction.isAnySelectMenu()) {
+      await executeMenu(interaction)
       return
     }
     if (interaction.isButton()) {
