e76bae6368e8cff5b5daa8635b706774ca03fc8c
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-04 16:02:08 -0300

    add menus to gobals

--- packages/bot/src/globals.d.ts
+++ packages/bot/src/globals.d.ts
@@ -4,8 +4,10 @@ import type { CommandNames } from './const/CommandNames'
 import type { Command } from './shared/BuildCommand'
 import type { ButtonNames } from './const/ButtonNames'
 import type { Button } from './shared/BuildButtons'
+import { type MenuNames } from './const/MenuMames'
 declare global {
   var commands: Collection<CommandNames | string, Command>
   var buttons: Collection<ButtonNames | string, Button>
+  var menus: Collection<MenuNames | string, Menu>
   var cooldowns: Collection<string, Collection<string, number>>
 }
