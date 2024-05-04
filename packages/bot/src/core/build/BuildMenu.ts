92d989319ea71f811326e97208116ca8c85e3f0d
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-04 16:01:21 -0300

    Create BuildMenu.ts

---
+++ packages/bot/src/core/build/BuildMenu.ts
@@ -0,0 +1,75 @@
+import PERMISSIONS_BASE from '../../const/PermissionsBase'
+import {
+  type InteractionEditReplyOptions,
+  type PermissionResolvable,
+  type StringSelectMenuBuilder,
+  type UserSelectMenuBuilder,
+  type RoleSelectMenuBuilder,
+  type MentionableSelectMenuBuilder,
+  type ChannelSelectMenuBuilder,
+  type StringSelectMenuInteraction,
+  type UserSelectMenuInteraction,
+  type RoleSelectMenuInteraction,
+  type MentionableSelectMenuInteraction,
+  type ChannelSelectMenuInteraction
+} from 'discord.js'
+import { type I18n } from '../../i18n'
+import { MENU_NAME, type MenuNames } from '@/const/MenuMames'
+interface Types {
+  string: {
+    builder: StringSelectMenuBuilder
+    interaction: StringSelectMenuInteraction
+  }
+  user: {
+    builder: UserSelectMenuBuilder
+    interaction: UserSelectMenuInteraction
+  }
+  role: {
+    builder: RoleSelectMenuBuilder
+    interaction: RoleSelectMenuInteraction
+  }
+  mentionable: {
+    builder: MentionableSelectMenuBuilder
+    interaction: MentionableSelectMenuInteraction
+  }
+  channels: {
+    builder: ChannelSelectMenuBuilder
+    interaction: ChannelSelectMenuInteraction
+  }
+}
+
+type MenuType = keyof Types
+interface MenuProps<T extends MenuType> {
+  menuType: T
+  name: MenuNames
+  ephemeral?: boolean
+  permissions: PermissionResolvable[]
+  cooldown?: number
+  data: Types[T]['builder']
+  execute: (e: Types[T]['interaction'], i18n: I18n) => Promise<InteractionEditReplyOptions>
+}
+/**
+ * #### Constructor
+ * * ` data `: The buttonBuilder.customId(name) not is required.
+ */
+class BuildMenu<T extends MenuType> {
+  type: 'menu' = 'menu'
+  name: string
+  ephemeral
+  permissions
+  cooldown
+  data
+  menuType: MenuType
+  execute
+  constructor(props: MenuProps<T>) {
+    this.name = MENU_NAME[props.name]
+    this.menuType = props.menuType
+    this.cooldown = props.cooldown ?? 0
+    this.ephemeral = props.ephemeral ?? false
+    this.permissions = [...new Set([...PERMISSIONS_BASE, ...props.permissions])]
+    this.data = props.data.setCustomId(this.name)
+    this.execute = props.execute
+  }
+}
+export type Menu = InstanceType<typeof BuildMenu>
+export default BuildMenu
