66075ba5a29385aa5ada77ed075bfe5feb98d8a1
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-05 15:41:43 -0300

    test menu Button

--- packages/bot/src/commands/tests/index.ts
+++ packages/bot/src/commands/tests/index.ts
@@ -1,6 +1,8 @@
-import { SlashCommandBuilder } from 'discord.js'
+import { ActionRowBuilder, SlashCommandBuilder, type StringSelectMenuBuilder } from 'discord.js'
 import BuildCommand from '@core/build/BuildCommand'
 import { CommandNamesKeys } from '../../const/CommandNames'
+import { MENU_NAME } from '@/const/MenuMames'
+import { type Menu } from '@/core/build/BuildMenu'
 
 export default new BuildCommand({
   cooldown: 60,
@@ -9,7 +11,10 @@ export default new BuildCommand({
   permissions: [],
   ephemeral: false,
   data: new SlashCommandBuilder().setDescription('Replies with Pong!'),
-  async execute(interaction) {
-    return { content: 'test!' }
+  async execute(interaction, i18n) {
+    const select: Menu = globalThis.menus.get(MENU_NAME.test)
+    console.log(select)
+    const row = new ActionRowBuilder<StringSelectMenuBuilder>({ components: [select.data] })
+    return { content: 'test!', components: [row] }
   }
 })
