1e0d2c30cdee035b59a36a2eb6c5ecf69a09b48b
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-10 17:55:13 -0300

    Update buildWelcomeImage.ts

--- packages/bot/src/shared/buildWelcomeImage.ts
+++ packages/bot/src/shared/buildWelcomeImage.ts
@@ -3,17 +3,17 @@ import { AttachmentBuilder, type GuildMember } from 'discord.js'
 
 // load Fonts
 import fonts from './fonts'
-import renderCanvas from './renderCanvas'
-import type { Base, Layer, TextBase, Canvas } from '@/types/Canvas.types'
+import renderCanvas from '@lib/renderCanvas'
+import type { Canvas } from '@/types/Canvas.types'
 for (const font of fonts) {
   GlobalFonts.registerFromPath(font.patch, font.nameAlias)
 }
 
 export default async function buildWelcomeImage(data: Canvas, member: GuildMember): Promise<AttachmentBuilder> {
   const user = {
-    avatar: member.avatarURL(),
-    count: member.guild.memberCount,
-    globalName: member.user.globalName,
+    avatar: member.avatarURL() ?? '',
+    count: member.guild.memberCount.toString(),
+    globalName: member.user.globalName ?? member.user.username,
     id: member.user.id,
     username: member.user.username
   }
@@ -21,11 +21,11 @@ export default async function buildWelcomeImage(data: Canvas, member: GuildMembe
   const canvas = createCanvas(data.width, data.height)
   const ctx = canvas.getContext('2d')
   await renderCanvas(
-    layers as unknown as Layer[],
-    base as unknown as Base & TextBase,
+    layers,
+    base,
     user,
     ctx as unknown as CanvasRenderingContext2D,
-    Path as unknown as typeof Path2D,
+    Path as typeof Path2D,
     loadImage as unknown as (path: string) => Promise<HTMLImageElement>
   )
   const extension = 'png'
