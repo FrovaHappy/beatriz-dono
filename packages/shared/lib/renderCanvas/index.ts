e1cb9e8114f447fa79d1342dbf95dc60dfc0870b
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-10 00:04:07 -0300

    copy renderCanvas to shared

---
+++ packages/shared/lib/renderCanvas/index.ts
@@ -0,0 +1,35 @@
+import { type Base, type Layer, type Text, type TextBase, type User, type Image, type Icon } from '@type/Canvas'
+import { renderText } from './renderText'
+import { renderImage } from './renderImage'
+import { renderIcon } from './renderIcon'
+
+export default async function renderCanvas(
+  layers: Layer[],
+  base: Base & TextBase,
+  user: User,
+  ctx: CanvasRenderingContext2D,
+  Path2DInstance: typeof Path2D,
+  loadImage: (path: string) => Promise<HTMLImageElement>
+) {
+  ctx.clearRect(0, 0, base.width, base.height)
+  ctx.save()
+  if (base.color) {
+    ctx.fillStyle = base.color
+    ctx.fillRect(0, 0, base.width, base.height)
+  }
+  ctx.restore()
+  for (const layer of layers) {
+    switch (layer.type) {
+      case 'text':
+        renderText(layer as Text, ctx, user, base)
+        break
+      case 'image':
+        await renderImage(layer as Image, ctx, base, loadImage)
+        break
+      case 'icon':
+        await renderIcon(layer as Icon, ctx, base, Path2DInstance, loadImage)
+        break
+    }
+  }
+  return ctx
+}
---
+++ packages/shared/lib/renderCanvas/renderText.ts
@@ -0,0 +1,28 @@
+import { type Base, type Text, type TextBase, type User } from '@type/Canvas'
+import formatterText from '../formatterText'
+
+export function renderText(text: Text, ctx: CanvasRenderingContext2D, user: User, base: Base & TextBase) {
+  const { x, y, size, family, weight, limitLetters, content, align, baseline, color } = text
+  let textContent = formatterText(content, {
+    userCount: user.count,
+    userGlobal: user.globalName,
+    userName: user.username,
+    userId: user.id
+  })
+  ctx.save()
+  ctx.font = `${weight} ${size}px ${family}`
+  ctx.textAlign = align
+  ctx.textBaseline = baseline
+  ctx.fillStyle = color ?? 'transparent'
+
+  let lengthLetters = ctx.measureText(textContent)
+  if (limitLetters > 0) {
+    while (limitLetters < lengthLetters.width) {
+      textContent = textContent.slice(0, -1)
+      lengthLetters = ctx.measureText(textContent)
+    }
+  }
+
+  ctx.fillText(textContent, x, y)
+  ctx.restore()
+}
