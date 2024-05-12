cbab91e3b301b18b6e5efe401ef7c4394d665065
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-12 10:39:04 -0300

    [wetsite] move sections dir to src

--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import styles from './page.module.scss'
-import Shapes from './sections/shapes'
+import Shapes from '../sections/shapes'
 import HomeContext from './context'
-import Options from './sections/options'
+import Options from '../sections/options'
 import NewShape from '@/components/NewShape'
 import Canvas from '@/components/Canvas'
-import GenerateJson from './sections/shapes/GenerateJson'
+import GenerateJson from '../sections/shapes/GenerateJson'
 export default function Home() {
   return (
     <HomeContext>
