d92db7fa782f7f59937a3819b8e99b05c3d23bde
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-12 11:05:28 -0300

    [bot] fix relative import of welcome

--- packages/bot/src/services/events/guildMemberAdd/welcome.ts
+++ packages/bot/src/services/events/guildMemberAdd/welcome.ts
@@ -1,7 +1,7 @@
 import { type GuildMember } from 'discord.js'
 import db from '@core/db'
 import SendWelcomeWith from '@/shared/sendWelcomeWith'
-import WELCOME from '@/const/welcome'
+import WELCOME from '@lib/welcome'
 import { validateCanvas } from '../../commands/setWelcome/validate'
 import { type Canvas } from '@/types/Canvas.types'
 import { formatterUser } from '@/services/shared/formatterUser'
