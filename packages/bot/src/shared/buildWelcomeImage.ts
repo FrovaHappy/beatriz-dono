860c9221cc15242481423107a7188cf42822f0df
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-08-21 00:35:55 -0300

    feat(bot): :bug: new renderCanvas module implemented

--- packages/bot/src/shared/buildWelcomeImage.ts
+++ packages/bot/src/shared/buildWelcomeImage.ts
@@ -4,14 +4,14 @@ import { AttachmentBuilder, type GuildMember } from 'discord.js'
 // load Fonts
 import fonts from './fonts'
 import renderCanvas from '@lib/renderCanvas'
-import type { Canvas } from '@/types/Canvas.types'
+import type { Canvas, Layer, Image } from '@/types/Canvas.types'
 for (const font of fonts) {
   GlobalFonts.registerFromPath(font.patch, font.nameAlias)
 }
 
 export default async function buildWelcomeImage(data: Canvas, member: GuildMember): Promise<AttachmentBuilder> {
   const user = {
-    avatar: member.avatarURL() ?? '',
+    avatar: member.avatarURL() ?? 'https://i.imgur.com/LB7cfKh.png',
     count: member.guild.memberCount.toString(),
     globalName: member.user.globalName ?? member.user.username,
     id: member.user.id,
@@ -20,14 +20,19 @@ export default async function buildWelcomeImage(data: Canvas, member: GuildMembe
   const { layers, ...base } = data
   const canvas = createCanvas(data.width, data.height)
   const ctx = canvas.getContext('2d')
-  await renderCanvas(
-    layers,
-    base,
-    user,
-    ctx as unknown as CanvasRenderingContext2D,
-    Path as typeof Path2D,
-    loadImage as unknown as (path: string) => Promise<HTMLImageElement>
-  )
+
+  const images: Record<string, HTMLImageElement> = {}
+  for (const l of layers) {
+    if (l.type === 'image') {
+      images[l.id] = (await loadImage(
+        (l as Layer<Image>).img ?? 'https://i.imgur.com/m8BHGOt.png'
+      )) as unknown as HTMLImageElement
+    }
+    if (l.type === 'icon') {
+      images[l.id] = (await loadImage(user.avatar)) as unknown as HTMLImageElement
+    }
+  }
+  renderCanvas(layers, base, user, ctx as unknown as CanvasRenderingContext2D, Path as typeof Path2D, images)
   const extension = 'png'
 
   return new AttachmentBuilder(await canvas.encode(extension), { name: `welcome.${extension}` })
