d40e5adc5847e65a7223e43c836a3d3e95dd8a95
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-09-14 11:10:00 -0300

    feat(bot): :sparkles: added removeRoles

---
+++ packages/bot/src/services/colors/shared/removeRoles.ts
@@ -0,0 +1,18 @@
+import { type Color } from '@prisma/client'
+import { Collection, type GuildMemberRoleManager, type Role } from 'discord.js'
+
+export default async function removeRoles(roles: Collection<string, Role> | undefined, colors: Color[], i: any) {
+  const colorsRemove = roles?.filter(role => colors.some(color => color.roleId === role.id)) ?? new Collection()
+  const logDeleteRoles = {
+    total: colorsRemove.size,
+    fails: [] as Role[]
+  }
+  for (const [, role] of colorsRemove) {
+    try {
+      await (i.member?.roles as GuildMemberRoleManager).remove(role)
+    } catch (error) {
+      logDeleteRoles.fails.push(role)
+    }
+  }
+  return logDeleteRoles
+}
