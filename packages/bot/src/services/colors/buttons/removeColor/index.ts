8a95d9af5f273df160bc0f6296df5fcbf195f0c3
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-09-14 11:19:59 -0300

    feat(bot): :sparkles: created removeColor buttons in /colors

---
+++ packages/bot/src/services/colors/buttons/removeColor/index.ts
@@ -0,0 +1,24 @@
+import { ButtonNames } from '@/const/interactionsNames'
+import BuildButton from '@/core/build/BuildButtons'
+import { ButtonBuilder, ButtonStyle } from 'discord.js'
+import fetchColorCommand from '../../shared/fetchColorCommand'
+import removeRoles from '../../shared/removeRoles'
+
+export default new BuildButton({
+  name: ButtonNames.removeColor,
+  permissions: [],
+  data: new ButtonBuilder().setLabel('Eliminar color').setStyle(ButtonStyle.Secondary),
+  async execute(i) {
+    const { guildId } = i
+    if (!guildId) return { content: 'No se encontró el guild' }
+    const roles = i.guild?.roles.cache
+    const { colorPointerId, colors } = await fetchColorCommand(guildId, roles)
+    if (!colorPointerId) return { content: 'No se encontró el puntero de colores' }
+
+    const logDeleteRoles = await removeRoles(roles, colors, i)
+    return {
+      content: `Se eliminaron ${logDeleteRoles.total} roles de colores, falló ${logDeleteRoles.fails.length} roles`,
+      components: []
+    }
+  }
+})
