8321ea01e0061cd90945dd22f91c400db09d45e5
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-09-14 11:21:55 -0300

    feat(bot): :sparkles: created defaultColor menu in /colors

---
+++ packages/bot/src/services/colors/menus/colorDefault/index.ts
@@ -0,0 +1,45 @@
+import COLORS from '@/const/colors'
+import { MenuNames } from '@/const/interactionsNames'
+import BuildMenu from '@/core/build/BuildMenu'
+import { type GuildMemberRoleManager, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js'
+import removeRoles from '../../shared/removeRoles'
+import createColorRole from '../../shared/createColorRole'
+import fetchColorCommand from '../../shared/fetchColorCommand'
+
+export default new BuildMenu({
+  name: MenuNames.colorDefault,
+  menuType: 'string',
+  defer: true,
+  permissions: [],
+  data: new StringSelectMenuBuilder({
+    placeholder: 'Selecciona un color'
+  }).addOptions(
+    ...COLORS.map(color => {
+      return new StringSelectMenuOptionBuilder().setValue(color.hexColor).setLabel(color.label).setEmoji(color.emoji)
+    })
+  ),
+  execute: async i => {
+    const { values, guildId } = i
+    const roles = i.guild?.roles.cache
+    if (!guildId) return { content: 'No se encontró el guild' }
+
+    const colorSelected = values[0]
+    const { colors, colorPointerId } = await fetchColorCommand(guildId, roles)
+    if (!colorPointerId) return { content: 'No se encontró el puntero de los colores' }
+
+    await removeRoles(roles, colors, i)
+    const colorRole = await createColorRole({
+      hexColor: colorSelected as `#${string}`,
+      guildId,
+      colors,
+      colorPointerId,
+      i
+    })
+    if (!colorRole) return { content: 'No se pudo crear el rol' }
+    // eslint-disable-next-line space-unary-ops
+    await (i.member?.roles as GuildMemberRoleManager).add(colorRole)
+    const position = roles?.get(colorPointerId)?.rawPosition ?? 0
+    await i.guild?.roles.setPosition(colorRole.id, position)
+    return { content: `El color de tu nickname se ha cambiado a <@&${colorRole.id}>` }
+  }
+})
