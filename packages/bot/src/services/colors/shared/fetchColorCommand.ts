12108f5a4a848ef2ec7119529a148c5a58f46052
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-09-14 11:10:50 -0300

    feat(bot): :sparkles: added fecthColorCommand

---
+++ packages/bot/src/services/colors/shared/fetchColorCommand.ts
@@ -0,0 +1,39 @@
+import db from '@/core/db'
+import { type Collection, type Role } from 'discord.js'
+
+/**
+ * This function is used to fetch the color command from the database.
+ * @param guildId the guild id of the server
+ * @param roles is o collection of roles of the server (required for compare the colorPointerId)
+ * @returns the color command from the database
+ */
+
+export default async function fetchColorCommand(guildId: string, roles?: Collection<string, Role>) {
+  let colorCommand = await db.colorCommand.findUnique({
+    where: { serverId: guildId },
+    include: { colors: true }
+  })
+  if (!colorCommand) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    colorCommand = (
+      await db.server.upsert({
+        where: { serverId: guildId },
+        create: {
+          serverId: guildId,
+          permissionsBot: ['public'],
+          colorCommand: {
+            create: {}
+          }
+        },
+        update: {
+          colorCommand: {
+            create: {}
+          }
+        },
+        include: { colorCommand: { include: { colors: true } } }
+      })
+    ).colorCommand!
+  }
+  if (colorCommand.colorPointerId) colorCommand.colorPointerId = roles?.get(colorCommand.colorPointerId)?.id ?? null
+  return colorCommand
+}
