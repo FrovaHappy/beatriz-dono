2392a5cfe9902a0072c602e6654b7fdd7bf28fbc
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-09-16 00:51:36 -0300

    refactor(bot): :recycle: add setSetting in db
    this to add/update the settings

--- packages/bot/src/core/db.ts
+++ packages/bot/src/core/db.ts
@@ -1,3 +1,31 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, type Setting } from '@prisma/client'
 const db = new PrismaClient()
+
+export async function setSetting(data: Partial<Setting>) {
+  const { config } = globalThis
+  let setting = await db.setting.findFirst()
+
+  if (!setting) {
+    await db.setting.create({
+      data: {
+        cooldown: data.cooldown ?? config.cooldown,
+        privatesServers: data.privatesServers ?? config.privatesServers,
+        ownersServers: data.ownersServers ?? config.ownersServers,
+        discordInviteUrl: data.discordInviteUrl ?? config.discordInviteUrl
+      }
+    })
+  } else {
+    setting = await db.setting.update({
+      where: { id: setting.id },
+      data: {
+        cooldown: data.cooldown ?? setting.cooldown ?? config.cooldown,
+        privatesServers: data.privatesServers ?? setting.privatesServers ?? config.privatesServers,
+        ownersServers: data.ownersServers ?? setting.ownersServers ?? config.ownersServers,
+        discordInviteUrl: data.discordInviteUrl ?? setting.discordInviteUrl ?? config.discordInviteUrl
+      }
+    })
+  }
+  globalThis.config = { ...config, ...setting }
+}
+
 export default db
