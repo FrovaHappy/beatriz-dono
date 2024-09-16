29c7160ca500dcf879f632c715c158f78adedd27
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-09-16 00:54:10 -0300

    refactor(bot): :recycle: refactor deployCommand to remove excess files

---
+++ packages/bot/src/core/deployCommands.ts
@@ -0,0 +1,59 @@
+import {
+  REST,
+  type RESTPostAPIChatInputApplicationCommandsJSONBody as CommandDataJson,
+  type Collection,
+  Routes
+} from 'discord.js'
+import { type Scope } from '@/types/main'
+import { type Command } from './build/BuildCommand'
+import pc from 'picocolors'
+
+export const rest = new REST().setToken(config.discordToken)
+
+export async function putGuildCommands(guildsIds: string[], commands: CommandDataJson[], scope: Scope) {
+  const errors: string[] = []
+  for await (const guildId of guildsIds) {
+    try {
+      await rest.put(Routes.applicationGuildCommands(config.discordClient, guildId), {
+        body: commands
+      })
+    } catch (error: any) {
+      errors.push(guildId)
+    }
+  }
+  console.log(
+    `· ${pc.bgBlue(scope)} commands done ${guildsIds.length} with ${errors.length} errors: [${errors.join(', ')}].`
+  )
+}
+
+export async function putGlobalCommands(commands: CommandDataJson[]) {
+  try {
+    await rest.put(Routes.applicationCommands(config.discordClient), {
+      body: commands
+    })
+  } catch (error: any) {
+    console.log(`· ${pc.bgBlue('public')} commands Fails: ${error.message}`)
+    return
+  }
+  console.log(`· ${pc.bgBlue('public')} commands Done `)
+}
+
+function getCommands(collection: Collection<string, Command>) {
+  const commands: Record<Scope, CommandDataJson[]> = {
+    owner: [],
+    private: [],
+    public: []
+  }
+  collection.forEach(command => {
+    commands[command.scope] = [command.data.toJSON(), ...(commands[command.scope] ?? [])]
+  })
+  return commands
+}
+
+export default async function deployCommand(collection: Collection<string, Command>): Promise<void> {
+  console.log('deployCommand start')
+  const commands = getCommands(collection)
+  await putGlobalCommands(commands.public) // update public commands
+  await putGuildCommands(config.ownersServers, commands.owner, 'owner') // update owner commands
+  await putGuildCommands(config.privatesServers, commands.private, 'private') // update private commands
+}
