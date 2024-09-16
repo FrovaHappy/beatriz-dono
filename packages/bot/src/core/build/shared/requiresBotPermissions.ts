7259595b58e23fab85ada09aae4878b278bd6720
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-09-16 00:58:56 -0300

    feat(bot): :sparkles: added requiereBotPermission function

---
+++ packages/bot/src/core/build/shared/requiresBotPermissions.ts
@@ -0,0 +1,25 @@
+import { type GuildMember, type PermissionResolvable, PermissionsBitField } from 'discord.js'
+
+interface Props {
+  permissions: PermissionResolvable[]
+  bot: GuildMember | null | undefined
+  nameInteraction: string
+  type: string
+}
+export default function requiresBotPermissions(props: Props) {
+  const { permissions, bot, nameInteraction, type } = props
+  if (!bot) return { content: 'No se encontró el bot' }
+
+  const permissionsCurrent = new PermissionsBitField(bot.permissions).toArray()
+
+  if (bot.permissions.has(permissions)) {
+    const permissionsRequired = new PermissionsBitField(permissions)
+      .toArray()
+      .filter(p => !permissionsCurrent.some(pc => pc === p))
+    return {
+      content: `El bot no tiene permisos para ejecutar el ${type} ${nameInteraction}, los permisos requeridos son ${permissionsRequired
+        .map(p => `\`${p}\``)
+        .join(', ')}`
+    }
+  }
+}
