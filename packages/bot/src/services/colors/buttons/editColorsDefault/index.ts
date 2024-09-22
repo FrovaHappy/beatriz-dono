04b6e41e36771d2d1d42d140b207440c9a3f380d
frova_happy <happypro5.0@gmail.com>
2024-09-22 19:33:14 -0300

    feat(Bot): :sparkles: add button edit-colors-default

---
+++ packages/bot/src/services/colors/buttons/editColorsDefault/index.ts
@@ -0,0 +1,19 @@
+import { ButtonNames } from '@/const/interactionsNames'
+import BuildButton from '@/core/build/BuildButtons'
+import { type Modal } from '@/core/build/BuildModal'
+import { ButtonBuilder, ButtonStyle } from 'discord.js'
+
+export default new BuildButton({
+  name: ButtonNames.editColorDefault,
+  data: new ButtonBuilder().setLabel('Edit colors default').setStyle(ButtonStyle.Secondary),
+  permissions: [],
+  defer: false,
+  execute: async i => {
+    const { modals } = globalThis
+    if (!i.guildId) return { content: 'Guild not found' }
+    const colorsDefaultModal: Modal = modals.get(i.customId)
+    if (!colorsDefaultModal) return { content: 'Modal not found' }
+
+    await i.showModal(colorsDefaultModal.data)
+  }
+})
