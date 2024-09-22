e18ee2e1a5df84738a018c564b083ef1f6139ddb
frova_happy <happypro5.0@gmail.com>
2024-09-22 19:30:38 -0300

    feat(Bot): :sparkles: added validations for the access of the scope

---
+++ packages/bot/src/core/build/shared/hasAccessForScope.ts
@@ -0,0 +1,11 @@
+import { type Scope } from '@/types/main'
+
+export default function hasAccessForScope(scope: Scope, guildId: string): boolean {
+  const { config } = globalThis
+  const isPrivate = config.privatesServers.includes(guildId)
+  const isOwner = config.ownersServers.includes(guildId)
+
+  if (scope === 'public') return false
+  if (scope === 'private') return isPrivate || isOwner // validate if the user is owner
+  return isOwner
+}
