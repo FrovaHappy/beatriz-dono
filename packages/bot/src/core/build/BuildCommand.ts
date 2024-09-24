0d6bf7b94ad0fcb5ece73c089fda45b8ec72cb96
frova_happy <happypro5.0@gmail.com>
2024-09-24 00:39:45 -0300

    refactor(Bot): :boom: changed contrat of BuildCommand and applied runInteraction

--- packages/bot/src/core/build/BuildCommand.ts
+++ packages/bot/src/core/build/BuildCommand.ts
@@ -1,50 +1,78 @@
 import PERMISSIONS_BASE from '../../const/PermissionsBase'
-import { type EventEmitted, type Scope } from '@/types/main'
-import type {
-  SlashCommandBuilder,
-  InteractionEditReplyOptions,
-  PermissionResolvable,
-  ChatInputCommandInteraction,
-  SlashCommandOptionsOnlyBuilder,
-  SlashCommandSubcommandsOnlyBuilder
+import { type MessageOptions, type Resolve, type Scope } from '@/types/main'
+import {
+  type SlashCommandBuilder,
+  type PermissionResolvable,
+  type ChatInputCommandInteraction,
+  type SlashCommandOptionsOnlyBuilder,
+  type SlashCommandSubcommandsOnlyBuilder,
 } from 'discord.js'
 
 import { type CommandNames } from '@/const/interactionsNames'
-
-interface CommandProps {
-  name: CommandNames
-  scope?: Scope
-  cooldown?: number
-  ephemeral?: boolean
-  defer?: boolean
-  permissions: PermissionResolvable[]
-  execute: (e: ChatInputCommandInteraction) => Promise<InteractionEditReplyOptions | undefined>
-  data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder | SlashCommandSubcommandsOnlyBuilder
-}
+import requiresBotPermissions from './shared/requiresBotPermissions'
+import isCooldownEnable from './shared/isCooldownEnable'
 /**
  * #### Constructor
  * * ` data `: The SlashCommandBuilder.setName(name) is Optional
  */
-class BuildCommand implements EventEmitted<string> {
+class BuildCommand {
   type: 'commands' = 'commands'
-  name
-  scope
-  defer
-  ephemeral
-  permissions
-  cooldown
-  data
-  execute
-  constructor(props: CommandProps) {
+  name: CommandNames
+  scope: Scope
+  ephemeral: boolean
+  permissions: PermissionResolvable[]
+  cooldown: number
+  data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder | SlashCommandSubcommandsOnlyBuilder
+  resolve: Resolve
+  execute: (e: ChatInputCommandInteraction) => Promise<MessageOptions | undefined>
+  constructor(props: Partial<BuildCommand> & Pick<BuildCommand, 'name' | 'execute' | 'data' | 'permissions'>) {
     this.name = props.name
     this.scope = props.scope ?? 'owner'
     this.cooldown = props.cooldown ?? 0
-    this.defer = props.defer ?? true
+    this.resolve = props.resolve ?? 'reply'
     this.ephemeral = props.ephemeral ?? false
     this.permissions = [...new Set([...PERMISSIONS_BASE, ...props.permissions])]
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     this.data = props.data.setName(this.name)
     this.execute = props.execute
+  }
+
+  static async runInteraction(i: ChatInputCommandInteraction) {
+    const command: Command = globalThis.commands.get(i.commandName)
+    if (!command) return { content: 'No se encontró el command' }
+
+    const messageRequirePermissions = requiresBotPermissions({
+      permissions: command.permissions,
+      bot: i.guild?.members.me,
+      nameInteraction: i.commandName,
+      type: 'command'
+    })
+    const messageCooldown = isCooldownEnable({
+      id: i.user.id,
+      cooldown: command.cooldown,
+      name: command.name,
+      type: 'command'
+    })
+    const getMessage = async () => {
+      if (messageRequirePermissions) return messageRequirePermissions
+      if (messageCooldown) return { content: messageCooldown }
+      try {
+        return await command.execute(i)
+      } catch (error) {
+        console.error(error)
+        return { content: `Error executing ${command.name}` }
+      }
+    }
+    try {
+      if (command.resolve === 'defer') await i.deferReply({ ephemeral: command.ephemeral })
+      const message = await getMessage()
+      if (!message) return
+      if (command.resolve === 'defer') return await i.editReply(message)
+      return await i.reply({ ...message, ephemeral: command.ephemeral })
+    } catch (error) {
+      console.error(error)
+      return { content: `Error executing ${command.name}` }
+    }
   }
 }
 
--- packages/bot/src/events/interactionsCreate/index.ts
+++ packages/bot/src/events/interactionsCreate/index.ts
@@ -1,31 +1,15 @@
 import { Events } from 'discord.js'
 import BuildEvent from '@core/build/BuildEvent'
-import executeRun from './executeRun'
 import BuildModal from '@/core/build/BuildModal'
 import BuildButton from '@/core/build/BuildButtons'
 import BuildMenu from '@/core/build/BuildMenu'
+import BuildCommand from '@/core/build/BuildCommand'
 export default new BuildEvent({
   name: Events.InteractionCreate,
   once: false,
   async execute(interaction) {
     if (interaction.isChatInputCommand()) {
-      await executeRun({
-        customNameEmitted: interaction.commandName,
-        type: 'commands',
-        locale: interaction.locale,
-        interaction,
-        deferReply: async options => {
-          await interaction.deferReply(options)
-        },
-        editReply: async options => {
-          await interaction.editReply(options)
-        },
-        reply: async options => {
-          await interaction.reply(options)
-        }
-      })
-
-      return
+      await BuildCommand.runInteraction(interaction)
     }
     if (interaction.isAnySelectMenu()) {
       await BuildMenu.runInteraction(interaction)
