81510b281c584678ba8213096b087686206be32b
frova_happy <happypro5.0@gmail.com>
2024-09-24 17:02:24 -0300

    chore(Bot): :fire: deleted executeRun and depencencies

--- packages/bot/src/events/interactionsCreate/filterOwnerCommands.ts
+++
@@ -1,8 +0,0 @@
-const { config } = globalThis
-/**
- * @returns `true` if the commands/buttons have access to execute
- */
-export default function filterOwnerCommands(scope: 'public' | 'private' | 'owner', userId: string): boolean {
-  if (scope !== 'owner') return true
-  return config.ownersServers.some(uid => userId === uid)
-}
