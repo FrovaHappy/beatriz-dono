7e0b938f6e15ba1d14bce0b6fc50855abfb3e3b2
frova_happy <happypro5.0@gmail.com>
2024-10-09 18:48:53 -0300

    feat(Bot): :sparkles: added reduceTupletoObj

--- packages/bot/src/shared/general.ts
+++ packages/bot/src/shared/general.ts
@@ -15,3 +15,32 @@ export function objWithKeyValueEqual<T extends string | number | symbol>(OBJ: Re
   }
   return obj as Record<T, T>
 }
+
+/**
+ * Separate a string by a separator
+ * @param tuple is a tuple of key and object were the key is the key of the object
+ * @param selectValue this select the value of the object. Ej: title select [tuple.object].title
+ * @returns return a new object with the tuple.key as key and the selectValue as value.
+ */
+export function reduceTupleToObj(
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  tuple: [key: string, object: Record<string, any>][],
+  selectValue: string
+): Record<string, string> {
+  const separe = selectValue.split('.')
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const recoveryKey = (ob: any, i = 0) => {
+    const value = ob[separe[i]]
+    const nextKey = separe[i + 1]
+    if (nextKey) return recoveryKey(value, i + 1)
+    return value
+  }
+
+  return tuple.reduce<Record<string, string>>((acc, elem) => {
+    const [k, obj] = elem
+    acc[k] = recoveryKey(obj)
+    console.log(acc)
+    return acc
+  }, {})
+}
