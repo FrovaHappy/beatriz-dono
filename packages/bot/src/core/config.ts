a76595b70d2c88ba793df0c1e80ac391bc725ff5
frova <happypro5.0@gmail.com>
2024-11-01 23:42:18 -0300

    build(root): :arrow_up: upgrade dependecies in root and ajust to builds

--- packages/bot/src/core/config.ts
+++ packages/bot/src/core/config.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import type { Setting } from '@prisma/client'
+import { z } from 'zod'
+import p from 'picocolors'
 
 const settingDb = {
   cooldown: 5, // 5 segundos
@@ -13,18 +15,35 @@ const settingDb = {
   linkTopgg: 'https://top.gg/bot/971562890702237766',
   linkBotList: 'https://discordbotlist.com/bots/beatrizdono-beta'
 } as Omit<Setting, 'id'>
-const env = {
-  // biome-ignore lint/style/noNonNullAssertion: <explanation>
-  discordToken: process.env.DISCORD_TOKEN!,
-  // biome-ignore lint/style/noNonNullAssertion: <explanation>
-  discordClient: process.env.DISCORD_CLIENT!,
-  // biome-ignore lint/style/noNonNullAssertion: <explanation>
-  discordOwner: process.env.DISCORD_OWNER!,
-  rootPath: process.env.NODE_ENV === 'production' ? `${process.cwd()}/dist` : `${process.cwd()}/src`
-}
+
 const utils = {
   imageTransparent: 'https://i.imgur.com/m8BHGOt.png',
   imageAvatar: 'https://i.imgur.com/LB7cfKh.png'
+}
+export const envSchema = z.object({
+  DISCORD_TOKEN: z.string(),
+  DISCORD_CLIENT: z.string(),
+  DISCORD_OWNER: z.string(),
+  DATABASE_URL: z.string()
+})
+const env = {
+  discordToken: process.env.DISCORD_TOKEN!,
+  discordClient: process.env.DISCORD_CLIENT!,
+  discordOwner: process.env.DISCORD_OWNER!,
+  rootPath: process.env.NODE_ENV === 'production' ? `${process.cwd()}/dist` : `${process.cwd()}/src`
+}
+
+try {
+  envSchema.parse(process.env)
+} catch (error) {
+  if (error instanceof z.ZodError) {
+    let message = `${p.bgRedBright(' Error ')} Invalid environment variables:\n\n`
+    for (const issue of error.issues) {
+      message += `${p.gray('┃')} ${issue.path.join('→')} ${p.gray(`(${issue.message})`)}\n`
+    }
+    console.log(message)
+    process.exit(1)
+  }
 }
 
 const config = { ...settingDb, ...env, ...utils }
