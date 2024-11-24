2c84c7e0ff8733ee127a5bc8115ee8fee52045f0
frova <happypro5.0@gmail.com>
2024-11-24 01:27:02 -0300

    chore(bot): :art: added message activity for the bot

--- packages/bot/src/events/ready/updateBot.ts
+++ packages/bot/src/events/ready/updateBot.ts
@@ -1,10 +1,23 @@
-import type { Client } from 'discord.js'
+import { ActivityType, type Client } from 'discord.js'
+import { version } from '../../../package.json'
 
 export default async function updateBot(client: Client): Promise<void> {
-  const time = 5 * 60 * 60 * 1000
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  setInterval(async () => {
-    const guilds = (await client.guilds.fetch()).size
-    client.user?.setActivity(`Dando soporte a ${guilds} servers...`)
-  }, time)
+  const time = 60 * 1000
+  const fetchGuilds = async () => {
+    const guilds = await client.guilds.fetch()
+    const guildsCount = guilds.size
+    const msgs = [
+      `En ${guildsCount} servidores.`,
+      `version ${version}`,
+      'Colores por doquier.',
+      'Votanos en top.gg',
+      'Desactivamos welcome messages'
+    ]
+    const random = Math.floor(Math.random() * msgs.length)
+
+    client.user?.setPresence({ activities: [{ name: msgs[random], type: ActivityType.Custom }] })
+  }
+
+  setInterval(fetchGuilds, time)
+  fetchGuilds()
 }
