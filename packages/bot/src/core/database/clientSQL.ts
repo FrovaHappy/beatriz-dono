7ed111997c8b24b43626cf99bb247606bd450a21
FrovaHappy <happypro5.0@gmail.com>
2025-02-01 16:15:25 -0300

    feat: add formatResponse function to process ResultSet and improve data handling

--- packages/bot/src/core/database/clientSQL.ts
+++ packages/bot/src/core/database/clientSQL.ts
@@ -1,5 +1,20 @@
-import type { Client } from '@libsql/client'
+import type { Client, ResultSet } from '@libsql/client'
 const { createClient } = require('@libsql/client')
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function formatResponse<T = Record<string, any>>(response: ResultSet): Array<T> {
+  const { rows, columns } = response
+  const data: Array<T> = []
+  for (const row of rows) {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    const rowData: Record<string, any> = {}
+    for (let i = 0; i < columns.length; i++) {
+      rowData[columns[i]] = row[i]
+    }
+    data.push(rowData as T)
+  }
+  return data
+}
 
 const client = createClient({
   url: config.env.sql.url,
