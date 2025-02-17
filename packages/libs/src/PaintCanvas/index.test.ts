65ccb9a099005cc126757092c383ee33e065ef4f
FrovaHappy <happypro5.0@gmail.com>
2025-02-16 22:38:18 -0300

    refactor: update filterText object with more descriptive values for improved clarity in tests

--- packages/libs/src/PaintCanvas/index.test.ts
+++ packages/libs/src/PaintCanvas/index.test.ts
@@ -20,17 +20,17 @@ beforeEach(async () => {
 })
 
 const filterText = {
-  userName: 'test',
+  userName: 'userName',
   userId: '1234567890',
   userDiscriminator: '1234',
-  userDisplayName: 'test',
+  userDisplayName: 'userName',
   userAvatar: 'https://i.pinimg.com/736x/d9/ba/9b/d9ba9b2f95bea749bc2c5289087fbdc4.jpg',
   userBanner: 'https://i.pinimg.com/736x/5d/9f/69/5d9f69a552b37e9909f9d825edc56f11.jpg',
-  membersCount: '1234567890',
+  membersCount: '343',
   guildAvatar: 'https://i.pinimg.com/736x/bc/a6/f6/bca6f6362ca065a41699a913ba0cedfe.jpg',
   guildBanner: 'https://i.pinimg.com/736x/02/f7/6b/02f76b4cf0d2ea81ea33e7573d8ef10b.jpg',
-  guildName: 'test',
-  guildId: '1234567890'
+  guildName: 'Server Name',
+  guildId: '46234567890'
 }
 const getImages = async (layers: Canvas['layers'], filterText: User & Guild) => {
   const images: Record<string, HTMLImageElement> = {}
