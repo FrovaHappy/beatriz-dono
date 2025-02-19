3ef65dc667d8463d847e018a2765dcff40791d12
FrovaHappy <happypro5.0@gmail.com>
2025-02-19 09:29:35 -0300

    refactor: add InteractionReplyOptions type for enhanced interaction handling

--- packages/bot/src/types/main.d.ts
+++ packages/bot/src/types/main.d.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Interaction, type PermissionResolvable } from 'discord.js'
+import { Interaction, type PermissionResolvable, type InteractionReplyOptions } from 'discord.js'
 
 export type Scope = 'public' | 'private' | 'owner'
 export type Resolve = 'update' | 'defer'
