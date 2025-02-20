5cfcc8fb0cb65d11c2e7b6dcae30a68e982a2c9d
FrovaHappy <happypro5.0@gmail.com>
2025-02-20 19:31:57 -0300

    refactor(bot): :art: implement color deletion options with improved user interaction and multilingual support

--- packages/bot/src/services/colors/settings/deleteOptions/menu.deleteAsomeColors.ts
+++ packages/bot/src/services/colors/settings/deleteOptions/menu.deleteAsomeColors.ts
@@ -1,26 +1,34 @@
-import { MenuNames } from '@/const/interactionsNames'
 import BuildMenu from '@/core/build/BuildMenu'
-import { RoleSelectMenuBuilder } from 'discord.js'
-import messages, { messagesColors } from '@/messages'
 import db from '@/database'
+import msgCreatePointerColor from '../../msg.createPointerColor'
 
 export default new BuildMenu<'role'>({
-  name: MenuNames.settingDeleteAsomeColors,
+  customId: 'settingsConfirmDeleteAsome',
+  maxValues: 10,
+  minValues: 1,
+  typeData: 'role',
+  translates: {
+    default: {
+      placeholder: 'Select the roles you want to delete'
+    },
+    'es-ES': {
+      placeholder: 'Selecciona los roles que quieres eliminar'
+    }
+  },
   ephemeral: true,
   resolve: 'update',
   scope: 'public',
   cooldown: 15,
-  data: new RoleSelectMenuBuilder().setMinValues(1).setMaxValues(10).setPlaceholder('Select a role'),
   permissionsBot: ['ManageRoles'],
   permissionsUser: ['ManageRoles'],
   async execute(i) {
     const { guildId, values, locale } = i
     const roles = i.guild?.roles.cache
-    if (!guildId) return messages.guildIdNoFound(locale)
+    if (!guildId) throw new Error('Guild ID not found')
 
     const { pointer_id, colors } = await db.colors.read(guildId)
     const colorPointerId = roles?.get(pointer_id ?? '0')?.id
-    if (!colorPointerId) return messagesColors.initColorPointer(locale)
+    if (!colorPointerId) return msgCreatePointerColor.getMessage(locale, {})
 
     const colorsRemove = colors.filter(
       color => roles?.get(color.role_id)?.id === values.find(value => value === color.role_id)
---
+++ packages/bot/src/services/colors/settings/deleteOptions/menu.index.ts
@@ -0,0 +1,60 @@
+import BuildMenu from '@/core/build/BuildMenu'
+import { Collection } from 'discord.js'
+import { removeRolesOfServer } from '../../shared/removeRoles'
+import db from '@/database'
+import msgCreatePointerColor from '../../msg.createPointerColor'
+import msgQuestDeleteAll from './msg.questDeleteAll'
+import msgQuestDeleteAsome from './msg.questDeleteAsome'
+
+enum DeleteColors {
+  deleteAll = 'delete-all',
+  deleteAsome = 'delete-asome',
+  deleteNotUsed = 'delete-not-used'
+}
+
+export default new BuildMenu<'string'>({
+  customId: 'settingsDeleteColors',
+  resolve: 'update',
+  permissionsBot: ['ManageRoles'],
+  permissionsUser: ['ManageRoles'],
+  typeData: 'string',
+  translates: {
+    default: {
+      placeholder: 'Delete the colors',
+      options: [
+        { label: 'Delete all colors', value: DeleteColors.deleteAll },
+        { label: 'Delete asome color', value: DeleteColors.deleteAsome },
+        { label: 'Delete colors not used', value: DeleteColors.deleteNotUsed }
+      ]
+    },
+    'es-ES': {
+      placeholder: 'Eliminar los colores',
+      options: [
+        { label: 'Eliminar todos los colores', value: DeleteColors.deleteAll },
+        { label: 'Eliminar algunos colores', value: DeleteColors.deleteAsome },
+        { label: 'Eliminar colores no utilizados', value: DeleteColors.deleteNotUsed }
+      ]
+    }
+  },
+  execute: async i => {
+    const { guildId, locale } = i
+    const value = i.values[0] as DeleteColors
+    const roles = i.guild?.roles.cache ?? new Collection()
+    if (!guildId) throw new Error('Guild ID not found')
+    const { colors, pointer_id } = await db.colors.read(guildId)
+    const colorPointerId = roles?.get(pointer_id ?? '0')?.id
+    if (!colorPointerId) return msgCreatePointerColor.getMessage(locale, {})
+
+    if (value === DeleteColors.deleteAll) return msgQuestDeleteAll.getMessage(locale, {})
+    if (value === DeleteColors.deleteAsome) return msgQuestDeleteAsome.getMessage(locale, {})
+    if (value === DeleteColors.deleteNotUsed) {
+      const colorsUnused = colors.filter(color => !roles.get(color.role_id))
+      const rolesUnused = colors.filter(color => roles.get(color.role_id)?.members.size === 0)
+      await removeRolesOfServer(roles, rolesUnused, i)
+      await db.colors.delete(guildId, [...colorsUnused, ...rolesUnused])
+      return {
+        content: `Se eliminaron ${rolesUnused.length + colorsUnused.length} roles de colores of ${colors.length} colores.`
+      }
+    }
+  }
+})
