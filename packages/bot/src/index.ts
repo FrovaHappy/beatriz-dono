4972f9dcd011c199520ca68f5db795181ca21cd1
FrovaHappy <happypro5.0@gmail.com>
2025-02-21 16:35:32 -0300

    fix(bot): :ambulance: integrate loadTables function and enhance error handling in the run process

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -2,9 +2,15 @@ import './paths'
 import './config'
 import startClient from './core/client'
 import getGuilds from './getGuilds'
+import { exit } from 'node:process'
+import { loadTables } from './database/clientSQL'
 
 const run = async () => {
+  await loadTables()
   await getGuilds()
   await startClient()
 }
-run()
+run().catch(error => {
+  console.error(error)
+  exit(1)
+})
