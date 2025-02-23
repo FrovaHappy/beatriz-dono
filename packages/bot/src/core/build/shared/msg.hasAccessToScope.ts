004b6b85e8dcce91a9935c85dadb90a5753517b4
FrovaHappy <happypro5.0@gmail.com>
2025-02-22 22:55:58 -0300

    fix(bot): :bug: fixed bug for BuildMessages contract change

--- packages/bot/src/core/build/shared/msg.hasAccessToScope.ts
+++ packages/bot/src/core/build/shared/msg.hasAccessToScope.ts
@@ -2,13 +2,15 @@ import BuildMessages from '@/core/build/BuildMessages'
 import { Colors } from 'discord.js'
 
 export default new BuildMessages({
-  default: {
-    embeds: [
-      {
-        title: 'Oh no!',
-        description: "You don't have access to this scope, is required the scope `{{slot0}}`",
-        color: Colors.Orange
-      }
-    ]
+  translates: {
+    default: {
+      embeds: [
+        {
+          title: 'Oh no!',
+          description: "You don't have access to this scope, is required the scope `{{slot0}}`",
+          color: Colors.Orange
+        }
+      ]
+    }
   }
 })
