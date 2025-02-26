5d28434a1321b120a59ca4dc80ae6e18ed58d9cd
FrovaHappy <happypro5.0@gmail.com>
2025-02-26 16:06:47 -0300

    feat(bot): :sparkles: add loading and drinkKanna emojis to the emoji constants

--- packages/bot/src/const/emojis.ts
+++ packages/bot/src/const/emojis.ts
@@ -8,7 +8,9 @@ const emojis = {
   sparkles: '<a:sparkles:1275991218383224832>',
   heartVar1: '<:heart:1275997495704686713>',
   githubIssue: '<:IssueOpen:1290130154336682137>',
-  colorWand: '<:ColorWand:1291563087198945300>'
+  colorWand: '<:ColorWand:1291563087198945300>',
+  loading: '<a:loadingLove:1343990282248650762>',
+  drinkKanna: '<a:DrinkKanna:1344374416305422398>'
 }
 export function getEmoji(emoji: keyof typeof emojis) {
   const emojiSelected = emojis[emoji]
