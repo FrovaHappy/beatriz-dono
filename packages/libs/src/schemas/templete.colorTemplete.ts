255d57038b2ea0ca6bc592353103f90ecb10c0a8
FrovaHappy <happypro5.0@gmail.com>
2025-02-27 18:52:47 -0300

    feat(bot): :sparkles: add color template schema with predefined colors and emojis

---
+++ packages/libs/src/schemas/templete.colorTemplete.ts
@@ -0,0 +1,17 @@
+import type { ColorsTempleteLatest } from './colorsTemplete'
+
+const colors: ColorsTempleteLatest = {
+  version: 'v1',
+  colors: [
+    { label: 'Black', hex_color: '#000000', emoji: '<:paleteBlack:1344776123648704628>' },
+    { label: 'White', hex_color: '#ffffff', emoji: '<:paleteWhite:1344776118657617931>' },
+    { label: 'Green', hex_color: '#00ff00', emoji: '<:paleteGreen:1344776116619186206>' },
+    { label: 'Blue', hex_color: '#0000ff', emoji: '<:paleteBlue:1344776128434409632>' },
+    { label: 'Red', hex_color: '#ff0000', emoji: '<:paleteRed:1344776125347532820>' },
+    { label: 'Yellow', hex_color: '#ffff00', emoji: '<:paleteYellow:1344776126907678802>' },
+    { label: 'Fuchsia', hex_color: '#ff00ff', emoji: '<:paleteFucsia:1344776122214383707>' },
+    { label: 'Cyan', hex_color: '#00ffff', emoji: '<:paleteCian:1344776120704434177>' }
+  ]
+}
+
+export default colors
