9655c31395f7238e49cff10d037f41b9352e1f1d
FrovaHappy <happypro5.0@gmail.com>
2025-02-28 16:00:34 -0300

    refactor(regex): :recycle: restructure regex exports to use a default object for better organization

--- packages/libs/src/regex.ts
+++ packages/libs/src/regex.ts
@@ -1,3 +1,9 @@
 export const regexHexColor = /^#([a-f0-9]{6})$/
 export const regexEmoji = /^<a?:([a-zA-Z0-9_]{2,32}):([0-9]{17,21})>$/
 export const regexRole = /^[0-9]{5,30}$/
+
+export default {
+  hexColor: regexHexColor,
+  emoji: regexEmoji,
+  role: regexRole
+}
