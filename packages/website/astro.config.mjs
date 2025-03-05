f2c508cb714fd37645707ad3d5154ac6fd578741
FrovaHappy <happypro5.0@gmail.com>
2025-03-05 18:24:36 -0300

    feat(docs): :sparkles: integrate starlight-theme-rapide for enhanced theming support

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,29 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
+import starlightThemeRapide from 'starlight-theme-rapide'
 import starlight from '@astrojs/starlight'
-
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), starlight({
-    title: 'Beatriz Dono',
-    defaultLocale: 'es',
-    locales: {
-      es: {
-        label: 'Español',
-        lang: 'es'
+  integrations: [
+    react(),
+    starlight({
+      plugins: [starlightThemeRapide()],
+      title: 'Beatriz Dono',
+      defaultLocale: 'es',
+      locales: {
+        es: {
+          label: 'Español',
+          lang: 'es'
+        },
+        en: {
+          label: 'English',
+          lang: 'en'
+        }
       },
-      en: {
-        label: 'English',
-        lang: 'en'
-      }
-    },
-    disable404Route: true,
-    editLink: { baseUrl: 'https://github.com/FrovaHappy/beatriz-dono/tree/main/packages/website/' },
-    customCss: [
-      'src/assets/docs.css'
-    ]
-  })]
+      disable404Route: true,
+      editLink: { baseUrl: 'https://github.com/FrovaHappy/beatriz-dono/tree/main/packages/website/' },
+      customCss: ['src/assets/docs.css']
+    })
+  ]
 })
