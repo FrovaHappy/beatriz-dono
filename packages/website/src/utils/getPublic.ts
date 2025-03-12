4aeadffdeff734d6a4bc29f42fcae637c3481f71
FrovaHappy <happypro5.0@gmail.com>
2025-03-12 19:02:29 -0300

    refactor(web): ♻️ simplify getPublic function by using a predefined Zod schema for validation

--- packages/website/src/utils/getPublic.ts
+++ packages/website/src/utils/getPublic.ts
@@ -1,20 +1,22 @@
 import { exit } from 'node:process'
 import { z } from 'zod'
 
+const schema = z.object({
+  urlApi: z.string(),
+  urlDiscord: z.string(),
+  urlOathCallback: z.string(),
+  urlGithub: z.string(),
+  urlJoinBot: z.string(),
+  urlDonate: z.object({
+    kofi: z.string()
+  })
+})
+
+export type EnvPublic = z.infer<typeof schema>
 export default function getPublic(string: string) {
   try {
     const data = JSON.parse(string)
-    return z
-      .object({
-        urlApi: z.string(),
-        urlDiscord: z.string(),
-        urlOathCallback: z.string(),
-        urlGithub: z.string(),
-        urlDonate: z.object({
-          kofi: z.string()
-        })
-      })
-      .parse(data)
+    return schema.parse(data)
   } catch (error) {
     if (error instanceof z.ZodError) {
       console.log(error.issues)
