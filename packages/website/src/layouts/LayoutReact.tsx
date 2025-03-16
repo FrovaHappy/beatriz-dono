f2e848ff62c85b26c69e3a67cc7c1b56f0006a58
FrovaHappy <happypro5.0@gmail.com>
2025-03-15 22:16:41 -0300

    refactor(web): ♻️ update children prop type in LayoutReact to use React.JSX.Element

--- packages/website/src/layouts/LayoutReact.tsx
+++ packages/website/src/layouts/LayoutReact.tsx
@@ -1,9 +1,10 @@
+import type React from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css'
 
 interface LayoutReactProps {
-  children: JSX.Element
+  children: React.JSX.Element
 }
 const id = 'iii'
 const toasts = {
