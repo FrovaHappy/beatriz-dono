d533614cdafc124a5372070b00e7e8eee979cb59
FrovaHappy <happypro5.0@gmail.com>
2025-03-15 21:58:54 -0300

    refactor(web): ✨ update module status to use structured object with text and color properties

--- packages/website/src/components/Landing/modules.ts
+++ packages/website/src/components/Landing/modules.ts
@@ -2,7 +2,10 @@ interface Modules {
   title: string
   description: string
   image: string
-  status: 'complete' | 'in progress' | 'planned'
+  status: {
+    text: string
+    color?: `#${string}`
+  }
   link: {
     url: string
     text: string
@@ -14,7 +17,10 @@ const mod: Modules[] = [
     title: 'Colores Infinitos',
     description: 'Personaliza tu nombre con cualquier color, puedes usar cualquier color que te lo cambio.',
     image: '/assets/feature_color.webp',
-    status: 'complete',
+    status: {
+      text: 'En Desarrollo',
+      color: '#6ceaf0'
+    },
     link: {
       url: '/modules/colors',
       text: 'como personalizar los nombres?'
@@ -25,7 +31,10 @@ const mod: Modules[] = [
     description:
       'Personaliza tus mensajes de bienvenida con un sistema que te permite personalizar como quieras, el limite es el cielo.',
     image: '/assets/feature_canvas.webp',
-    status: 'in progress',
+    status: {
+      text: 'En Desarrollo',
+      color: '#6ceaf0'
+    },
     link: {
       url: '/build-canvas/beforeofstart',
       text: 'Como crear un canvas?'
@@ -35,7 +44,10 @@ const mod: Modules[] = [
     title: 'Sistema de Ranking',
     description: 'Impulsa la actividad de tu servidor, con un ranking que te permite ver quién es el mejor.',
     image: '/assets/feature_ranking.webp',
-    status: 'planned',
+    status: {
+      text: 'Planeado',
+      color: '#e8cf07'
+    },
     link: {
       url: '/modules/ranking',
       text: 'Como crear un canvas?'
