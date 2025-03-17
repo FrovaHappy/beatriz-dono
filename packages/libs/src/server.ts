f8d65a06de7b90d7a785c85eeb1b1a4830facf53
FrovaHappy <happypro5.0@gmail.com>
2025-03-16 21:37:59 -0300

    feat(server): ✨ add getImageData function to fetch and process image data

---
+++ packages/libs/src/server.ts
@@ -0,0 +1,13 @@
+import { createCanvas, loadImage } from '@napi-rs/canvas'
+
+export const getImageData = async (url: string) => {
+  const dataFetch = await fetch(url).then(async res => (await res.blob()).arrayBuffer())
+
+  const data = await loadImage(dataFetch)
+  const canvas = createCanvas(data.width, data.height)
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(data, 0, 0, data.width, data.height)
+  const imageData = ctx.getImageData(0, 0, data.width, data.height)
+
+  return imageData.data
+}
