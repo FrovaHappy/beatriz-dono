a1810b05dc41e1784f6f7ff6064a6d829f0425ef
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-04-17 00:18:39 -0300

    refactor: comment out unused imports and command structure in index.ts

--- packages/bot/src/services/setWelcome/commands/main/index.ts
+++ packages/bot/src/services/setWelcome/commands/main/index.ts
@@ -1,80 +1,80 @@
-import { formattedErrorZod } from '@/shared/formattedErrorZod'
-import { toJson } from '@/shared/general'
-import SendWelcomeWith from '@/shared/sendWelcomeWith'
-import BuildCommand from '@core/build/BuildCommand'
-import { validateCanvas } from '@libs/PaintCanvas/schema.welcome.v1'
-import WELCOME from '@libs/PaintCanvas/template.welcome'
-import formatterText from '@libs/formatterText'
-import { Colors, EmbedBuilder, type GuildMember, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js'
+// import { formattedErrorZod } from '@/shared/formattedErrorZod'
+// import { toJson } from '@/shared/general'
+// import SendWelcomeWith from '@/shared/sendWelcomeWith'
+// import BuildCommand from '@core/build/BuildCommand'
+// import { validateCanvas } from '@libs/PaintCanvas/schema.welcome.v1'
+// import WELCOME from '@libs/PaintCanvas/template.welcome'
+// import formatterText from '@libs/formatterText'
+// import { Colors, EmbedBuilder, type GuildMember, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js'
 
-export default new BuildCommand({
-  cooldown: 0,
-  name: 'welcome',
-  ephemeral: true,
-  scope: 'owner',
-  data: new SlashCommandBuilder()
-    .setDescription('Set the welcome message')
-    .setDescriptionLocalizations({
-      'es-ES': 'Establece el mensaje de bienvenida',
-      'es-419': 'Establece el mensaje de bienvenida'
-    })
-    .addChannelOption(op =>
-      op.setName('channel').setDescription('channel where the message will be sent').setRequired(true)
-    )
-    .addStringOption(op =>
-      op
-        .setName('send')
-        .setDescription('send the message with')
-        .addChoices({ name: 'All', value: 'all' })
-        .setRequired(true)
-    )
-    .addStringOption(op => op.setName('message').setDescription('message to send').setRequired(false))
-    .addStringOption(op => op.setName('image').setDescription('image to send').setRequired(false))
-    .setDefaultMemberPermissions(
-      PermissionFlagsBits.Administrator | PermissionFlagsBits.ManageChannels | PermissionFlagsBits.ManageMessages
-    ),
-  async execute(i) {
-    const serverId = i.guild?.id
-    if (!serverId) return { content: 'error with server id' }
-    const image = toJson(i.options.getString('image')) ?? WELCOME
-    const message = i.options.getString('message') ?? ' welcome to {{server_name}} {{user_name}}'
-    const channelId = i.options.getChannel('channel', true).id
-    const send = i.options.getString('send', true)
-    const validate = validateCanvas(image)
-    console.log({ validate, image })
-    if (!validate.ok) {
-      if (!validate.errors) return { content: 'error in JSON' }
-      return {
-        embeds: [
-          new EmbedBuilder({
-            title: 'error in JSON',
-            description: `detected an error in the JSON: ${formattedErrorZod(validate.errors)}`,
-            color: Colors.Red
-          })
-        ]
-      }
-    }
-    const messageReply = formatterText(message, {
-      '{{server_count}}': i.guild?.memberCount.toString(),
-      '{{server_name}}': i.guild?.name,
-      '{{user_name}}': i.member?.user.username,
-      '{{user_id}}': i.member?.user.id,
-      '{{user_discriminator}}': i.member?.user.discriminator
-    })
-    return {
-      embeds: [
-        new EmbedBuilder({
-          title: 'success',
-          color: Colors.Aqua,
-          description: 'welcome message updated'
-        })
-      ],
-      ...(await SendWelcomeWith({
-        image: image,
-        message: messageReply,
-        member: i.member as GuildMember,
-        send
-      }))
-    }
-  }
-})
+// export default new BuildCommand({
+//   cooldown: 0,
+//   name: 'welcome',
+//   ephemeral: true,
+//   scope: 'owner',
+//   data: new SlashCommandBuilder()
+//     .setDescription('Set the welcome message')
+//     .setDescriptionLocalizations({
+//       'es-ES': 'Establece el mensaje de bienvenida',
+//       'es-419': 'Establece el mensaje de bienvenida'
+//     })
+//     .addChannelOption(op =>
+//       op.setName('channel').setDescription('channel where the message will be sent').setRequired(true)
+//     )
+//     .addStringOption(op =>
+//       op
+//         .setName('send')
+//         .setDescription('send the message with')
+//         .addChoices({ name: 'All', value: 'all' })
+//         .setRequired(true)
+//     )
+//     .addStringOption(op => op.setName('message').setDescription('message to send').setRequired(false))
+//     .addStringOption(op => op.setName('image').setDescription('image to send').setRequired(false))
+//     .setDefaultMemberPermissions(
+//       PermissionFlagsBits.Administrator | PermissionFlagsBits.ManageChannels | PermissionFlagsBits.ManageMessages
+//     ),
+//   async execute(i) {
+//     const serverId = i.guild?.id
+//     if (!serverId) return { content: 'error with server id' }
+//     const image = toJson(i.options.getString('image')) ?? WELCOME
+//     const message = i.options.getString('message') ?? ' welcome to {{server_name}} {{user_name}}'
+//     const channelId = i.options.getChannel('channel', true).id
+//     const send = i.options.getString('send', true)
+//     const validate = validateCanvas(image)
+//     console.log({ validate, image })
+//     if (!validate.ok) {
+//       if (!validate.errors) return { content: 'error in JSON' }
+//       return {
+//         embeds: [
+//           new EmbedBuilder({
+//             title: 'error in JSON',
+//             description: `detected an error in the JSON: ${formattedErrorZod(validate.errors)}`,
+//             color: Colors.Red
+//           })
+//         ]
+//       }
+//     }
+//     const messageReply = formatterText(message, {
+//       '{{server_count}}': i.guild?.memberCount.toString(),
+//       '{{server_name}}': i.guild?.name,
+//       '{{user_name}}': i.member?.user.username,
+//       '{{user_id}}': i.member?.user.id,
+//       '{{user_discriminator}}': i.member?.user.discriminator
+//     })
+//     return {
+//       embeds: [
+//         new EmbedBuilder({
+//           title: 'success',
+//           color: Colors.Aqua,
+//           description: 'welcome message updated'
+//         })
+//       ],
+//       ...(await SendWelcomeWith({
+//         image: image,
+//         message: messageReply,
+//         member: i.member as GuildMember,
+//         send
+//       }))
+//     }
+//   }
+// })
