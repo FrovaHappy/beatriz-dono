59d6319770e486f16b25b42302045a6ff838a391
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-04-17 15:37:59 -0300

    refactor: optimize getImageData function by removing unnecessary data fetch and improving error handling

--- packages/libs/src/server.ts
+++ packages/libs/src/server.ts
@@ -3,9 +3,9 @@ import paintCanvas from '@libs/PaintCanvas'
 
 export const getImageData = async (url: string | null) => {
   if (!url) return null
-  const dataFetch = await fetch(url).then(async res => (await res.blob()).arrayBuffer())
 
-  const data = await loadImage(dataFetch)
+  const data = await loadImage(url, { requestOptions: { timeout: 10_000 } }).catch(e => null)
+  if (!data) return null
   const canvas = createCanvas(data.width, data.height)
   const ctx = canvas.getContext('2d')
   ctx.drawImage(data, 0, 0, data.width, data.height)
