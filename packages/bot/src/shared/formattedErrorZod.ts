e21bd712ee0521ff66bbf34b49bcf0fd0a0d73c1
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-04-17 00:18:03 -0300

    refactor: enhance documentation for formattedErrorZod function

--- packages/bot/src/shared/formattedErrorZod.ts
+++ packages/bot/src/shared/formattedErrorZod.ts
@@ -1,5 +1,18 @@
 import type { z } from 'zod'
-
+/**
+ *
+ * @param error
+ * @param rootPath
+ * @returns returns a string with the error message formatted
+ * example:
+ * ```
+ * `
+ *  - \`[0].name\`: "name" is required
+ *  - \`[0].name\`: "name" must be a string
+ *  - \`[0].name\`: "name" must be at least 1 character long
+ * `
+ * ```
+ */
 export function formattedErrorZod(error: z.ZodError['errors'] | undefined, rootPath = '') {
   if (!error) return 'an exception occurred'
   let errorMessage = ''
