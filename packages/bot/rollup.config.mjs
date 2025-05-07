34749a9efcdf038b5360e9555a2ba46895327511
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-05-07 16:58:55 -0300

    refactor: remove unused typescriptPaths plugin and update output configuration

--- packages/bot/rollup.config.mjs
+++ packages/bot/rollup.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from 'rollup'
 import typescript from 'rollup-plugin-typescript2'
 import json from '@rollup/plugin-json'
-import { typescriptPaths } from 'rollup-plugin-typescript-paths'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import commonjs from '@rollup/plugin-commonjs'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   input: 'src/index.ts',
   output: {
     file: 'dist/index.js',
-    format: 'es'
+    format: 'es',
+    sourcemap: true
   },
-  external: ['discord.js', '@napi-rs/canvas', '@libs/*'],
+  external: ['discord.js', '@napi-rs/canvas'],
 
-  plugins: [typescript(), typescriptPaths(), json(), commonjs()]
+  plugins: [typescript(), commonjs(), json()]
 })
