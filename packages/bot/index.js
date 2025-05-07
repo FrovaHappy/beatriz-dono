f746babc51c0c5ede7c22cb2a04d0b84e48ab39b
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-05-07 17:00:26 -0300

    refactor: remove module-alias integration and streamline tsconfig paths registration

--- packages/bot/index.js
+++ packages/bot/index.js
@@ -1,8 +1,11 @@
-import { addAliases } from 'module-alias'
-
-addAliases({
-  '@libs': '../libs/dist'
-})
+import tsConfig from './tsconfig.json'
+import tsConfigPaths from 'tsconfig-paths'
 ;(async () => {
+  const baseUrl = './dist'
+  const cleanup = tsConfigPaths.register({
+    baseUrl,
+    paths: tsConfig.compilerOptions.paths
+  })
+  cleanup()
   await import('./dist/index.js')
 })()
