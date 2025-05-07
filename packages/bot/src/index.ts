89c890c823da47111e70cfb4d8be0e4c6474469b
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-05-07 18:34:17 -0300

    refactor: remove unused module-alias integration and clean up package dependencies

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -1,16 +1,10 @@
 import './config'
-import moduleAlias from 'module-alias'
 import { exit } from 'node:process'
 import startClient from './core/client'
 import getGuilds from './getGuilds'
 import { syncCli } from './database/clientSQL'
-import path from 'node:path'
 
 const run = async () => {
-  console.log(path.join(process.cwd(), '../libs/dist'))
-  moduleAlias.addAliases({
-    '@libs': path.join(process.cwd(), '../libs/dist')
-  })
   await syncCli()
   await getGuilds()
   await startClient()
