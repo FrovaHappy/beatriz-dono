d618ee35b150a68c25096849e81e483698878492
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-05-07 16:57:06 -0300

    refactor: simplify event loading by removing file path handling and using listImports directly

--- packages/bot/src/core/getEvents.ts
+++ packages/bot/src/core/getEvents.ts
@@ -1,20 +1,12 @@
-import path from 'node:path'
-import { readAllFiles, Timer } from '@/shared/general'
+import { Timer } from '@/shared/general'
 import { type ClientEvents, Collection } from 'discord.js'
 import type BuildEvent from './build/BuildEvent'
 import * as listImports from '@/listImports'
 import logger from '@/shared/logger'
 
-const includes = ['.ts', '.js']
-const excludes = ['.test.ts', '.d.ts', '.test.js']
-
 export default async function getEvents() {
   const time = new Timer()
   const events = new Collection<string, BuildEvent<keyof ClientEvents>>()
-  const absolutePath = path.join(config.env.rootPath, 'events')
-  const eventsPath = (await readAllFiles(absolutePath)).filter(
-    file => includes.some(include => file.endsWith(include)) && !excludes.some(exclude => file.endsWith(exclude))
-  )
   for (const event of Object.entries(listImports.events)) {
     const [, instance] = event
     events.set(instance.name, instance)
@@ -25,7 +17,7 @@ export default async function getEvents() {
     title: 'Loading events',
     body: `
       events found: ${events.size}  
-      files scanned: ${eventsPath.length}  
+      files scanned: ${listImports.events.length}  
       finished in ${time.final()}
     `
   })
