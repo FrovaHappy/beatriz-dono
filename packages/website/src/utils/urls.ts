b3dab57cb37bdca2f50d6d19d01f56c73ad5f313
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-05-09 00:02:46 -0300

    fix: update redirect URI in oAuth URL for Discord integration

--- packages/website/src/utils/urls.ts
+++ packages/website/src/utils/urls.ts
@@ -1,10 +1,11 @@
 const { PUBLIC_APPLICATION_ID: applicationId, PUBLIC_URL_API: urlApi } = import.meta.env
+console.log(import.meta.env)
 
 const urls = {
   github: 'https://github.com/FrovaHappy/beatriz-dono',
   discord: 'https://discord.gg/JRpHsGC8YQ',
   joinBot: `https://discord.com/oauth2/authorize?client_id=${applicationId}&permissions=8&integration_type=0&scope=bot`,
-  oAuth: `https://discord.com/oauth2/authorize?client_id=${applicationId}&response_type=code&redirect_uri=${encodeURIComponent(`${urlApi}/callback/discord`)}&scope=email+guilds+identify`,
+  oAuth: `https://discord.com/oauth2/authorize?client_id=${applicationId}&response_type=code&redirect_uri=${encodeURIComponent(`${urlApi}/auth0/discord`)}&scope=email+guilds+identify`,
   api: urlApi,
   kofi: 'https://ko-fi.com/frovahappy'
 } as const
