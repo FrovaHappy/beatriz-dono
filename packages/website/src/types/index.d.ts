d8befa1f4200c455d9448e58dceea81d94b72196
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2025-05-13 22:54:57 -0300

    feat: add color.svg asset for color representation

--- packages/website/src/types/index.d.ts
+++ packages/website/src/types/index.d.ts
@@ -2,3 +2,13 @@ export interface ComponentProps {
   apiUrl: string
   oauthCallback: string
 }
+
+export interface ButtonProps {
+  text?: string
+  id?: string
+  action: {
+    type: 'anchor' | 'button'
+    text: string
+  }
+  icon?: keyof typeof icons
+}
